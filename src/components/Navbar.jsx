e96c790733c62e9dde67622dc6bfcb00d103d3f8
Sushanta Bhowmick <bhosushanta922@gmail.com>
2022-06-20 01:23:15 +0530

    modidied path

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = ({ showSearch = false }) => {
             <img src="images/logo.png" alt="Addressbook" />
           </div>
           <li>
-            <a href="#">Home</a>
+            <a href="/">Home</a>
           </li>
           <li>
             <a href="#about">About</a>
